d3455bc6dbde3fd5b3b10b835d412fcd48aa9b00
unknown <engel199702@hotmail.com>
2019-02-26 20:54:52 -0600

    setup for windows

--- ingresousuarios/ingresousuarios/settings.py
+++ ingresousuarios/ingresousuarios/settings.py
@@ -83,7 +83,14 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
-   
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ingresousuariosproteccion',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
 }
 
 
